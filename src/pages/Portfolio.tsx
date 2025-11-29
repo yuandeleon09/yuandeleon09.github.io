dc78efcc357207e6e95b35f06f12c0299f3c747e
Alchemist <142375479+yuandeleon09@users.noreply.github.com>
2025-11-29 12:20:11 +0800

    Delete src directory

--- src/pages/Portfolio.tsx
+++
@@ -1,8 +0,0 @@
-export default function Portfolio() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>My Portfolio</h1>
-      <p>Welcome to my portfolio!</p>
-    </div>
-  );
-}
--- src/posts/first-post.md
+++
@@ -1,4 +0,0 @@
-# My First Blog Post
-
-Welcome to my blog!  
-This content is loaded from a .md file.
